c010869cbb69e117739583edc7b76991542ec5fe
alex-yellow <hitys2000@gmail.com>
2023-09-30 13:54:16 +0300

    add lesson1

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', function(req,res){
+    res.send('Hello world!');
+});
+
+app.listen(PORT, function(){
+    console.log('Server is running on port', PORT);
+});
